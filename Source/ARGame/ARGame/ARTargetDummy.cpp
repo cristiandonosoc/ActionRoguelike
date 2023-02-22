df9abf8a8b054180e12b61296e422a7bfd55d0f6
Cristian Donoso <cristiandonosoc@gmail.com>
2023-02-21 20:11:07 -0500

    [Lecture 8] Add Materials + Animated Health Bar using Materials
    This adds a couple of sample materials, which are animated using time
    (and our correct |GetAccurateTime| blueprint function).
    
    We now have an animated healthbar that flashes to the hit chunk when
    damage occurs.

---
+++ Source/ARGame/ARGame/ARTargetDummy.cpp
@@ -0,0 +1,44 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "ARTargetDummy.h"
+
+
+// Sets default values
+AARTargetDummy::AARTargetDummy()
+{
+	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if
+	// you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	Mesh = CreateDefaultSubobject<UStaticMeshComponent>("Mesh");
+	RootComponent = Mesh;
+
+	Attributes = CreateDefaultSubobject<UARAttributeComponent>("Attributes");
+	Attributes->OnHealthChanged.AddDynamic(this, &AARTargetDummy::OnHealthChanged);
+
+}
+
+// Called when the game starts or when spawned
+void AARTargetDummy::BeginPlay()
+{
+	Super::BeginPlay();
+}
+void AARTargetDummy::OnHealthChanged(FOnHealthChangedPayload payload)
+{
+	UE_LOG(LogTemp, Log, TEXT("HEALTH CHANGED: %f"), payload.NewHealth);
+
+	// We only flash on negative hits.
+	if (payload.Delta > 0)
+	{
+		return;
+	}
+
+	Mesh->SetScalarParameterValueOnMaterials("TimeToHit", GetWorld()->TimeSeconds);
+}
+
+// Called every frame
+void AARTargetDummy::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+}
---
+++ Source/ARGame/ARGame/ARTargetDummy.h
@@ -0,0 +1,40 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "ARAttributeComponent.h"
+
+#include "ARTargetDummy.generated.h"
+
+class UARAttributeComponent;
+
+class UStaticMeshComponent;
+
+UCLASS()
+class ARGAME_API AARTargetDummy : public AActor
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this actor's properties
+	AARTargetDummy();
+
+	// Called every frame
+	virtual void Tick(float delta) override;
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+	UFUNCTION()
+	void OnHealthChanged(FOnHealthChangedPayload payload);
+
+protected:
+	UPROPERTY(VisibleAnywhere)
+	TObjectPtr<UARAttributeComponent> Attributes;
+
+	UPROPERTY(VisibleAnywhere)
+	TObjectPtr<UStaticMeshComponent> Mesh;
+};
