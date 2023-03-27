6562a20537484f93e6f4f2e5c44b5fe26672a2e2
Cristián Donoso <cristiandonosoc@gmail.com>
2023-03-26 21:25:46 -0400

    [debug] Add ALWAYS debug category
    This is meant to always be on and useful for quick debugging

---
+++ Source/ARGame/ARGame/ARDebugCategories.cpp
@@ -0,0 +1,4 @@
+﻿#include <ARGame/ARDebugCategories.h>
+
+AR_DECLARE_DEBUG_CATEGORY(ALWAYS, ARDebugCategories::ALWAYS, true,
+						  "General categories that should be always on.");
--- Source/ARGame/ARGame/ARDebugCategories.h
+++ Source/ARGame/ARGame/ARDebugCategories.h
@@ -3,13 +3,26 @@
 // ReSharper disable once CppUnusedIncludeDirective
 #include <ARBase/DebugDraw.h>
 
+// We use this macro to ensure each category will get a different number (as long as they are
+// each defined in a separate line).
+//
+// IMPORTANT: This means that ids *WILL NOT* be stable across builds (if the categories are
+//            changed). If there is ever a need for that, then another strategy for defining
+//            categories ids are needed.
+#define UNIQUE_CATEGORY_ID __LINE__
+
 // ARDebugCategories tracks all the categories to be used in this module.
 // Make sure these categories don't collide.
 class ARDebugCategories
 {
 public:
-	static constexpr uint32 ACTIONS = 0;
-	static constexpr uint32 INTERACTION = 1;
-	static constexpr uint32 PLAYER_CHARACTER = 2;
-	static constexpr uint32 PROJECTILES = 3;
+	// ALWAYS is special in that it will always work if there is debug support enable.
+	// This still won't work on release builds.
+	static constexpr uint32 ALWAYS = UNIQUE_CATEGORY_ID;
+	static constexpr uint32 ACTIONS = UNIQUE_CATEGORY_ID;
+	static constexpr uint32 INTERACTION = UNIQUE_CATEGORY_ID;
+	static constexpr uint32 PLAYER_CHARACTER = UNIQUE_CATEGORY_ID;
+	static constexpr uint32 PROJECTILES = UNIQUE_CATEGORY_ID;
 };
+
+#undef UNIQUE_CATEGORY_ID
